eb7d85feb21fd94fd2696f7a63f75cd4ecfc8c48
Sobhan <Sobhan.kasa@gmail.com>
2023-04-19 17:12:49 +0430

    creating base

--- P1.py
+++ P1.py
@@ -1,10 +1,24 @@
-# project1 A
 class Inventory:
 
 class Customer:
 
 class Admin:
-    def __init__(self, mokhtalet) :
-        self.mokh = mokhtalet
-        
 
+class Product:
+
+
+class Cart:
+
+
+class Pay:
+    def __init__(self, card_number, cvv2, expire_date, gmail= None):
+        self.code_peygiri = None
+        self.tarikh_sabt_sefaresh = None
+        if confirm:
+            self.code_peygiri = np.random()
+            self.tarikh_sabt_sefaresh = time.now()
+class Address:
+    def __init__(self, sharestan, shahr, baze_tahvil, addrees_koli, code_posti, pelak, vahed, phone_number, tahvil_girande):
+
+class Factor:
+    def __init__(self, gheymat, ajnas, address,tarikh_sabt_sefaresh ):
