b879b6f715f62b42ee67de73f5c0702653a3f4f9
Sobhan <Sobhan.kasa@gmail.com>
2023-04-19 17:32:16 +0430

    selfs have been completed in Pay class

--- P1.py
+++ P1.py
@@ -9,9 +9,13 @@ class Product:
 
 class Cart:
 
-
+""" pay gharare oon safhe vared kardane shomare card va takmil farayand kharid ro shabih sazi kone"""
 class Pay:
-    def __init__(self, card_number, cvv2, expire_date, gmail= None):
+    def __init__(self, card_number, cvv2, expire_date, gmail = None):
+        self.card_number = card_number
+        self.cvv2 = cvv2
+        self.expire_date = expire_date
+        self.gmail = gmail
         self.code_peygiri = None
         self.tarikh_sabt_sefaresh = None
         if confirm:
