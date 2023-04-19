e978c2244c954d4fc32d367b9947c128443d276f
Sobhan <Sobhan.kasa@gmail.com>
2023-04-19 19:47:55 +0430

    add validate email function

--- P1.py
+++ P1.py
@@ -1,4 +1,4 @@
-
+from random import randint
 
 class Inventory:
 
@@ -18,16 +18,20 @@ def random_number_with_n_digits(n):
     range_start = 10**(n-1)
     range_end = (10**n)-1
     return randint(range_start, range_end)
+
+def validate_email(email):
+    return bool(re.match(r"^[a-zA-Z0-9\.\_]+@((gmail)|(yahoo)|(outlook)|(hotmail)|(live)|([a-z]*\.*[a-z]+\.ac)|(chmail))\.((com)|(ir))$", email, re.IGNORECASE))
 class Pay:
     def __init__(self, card_number, cvv2, expire_date, gmail = None):
         self.card_number = card_number
         self.cvv2 = cvv2
         self.expire_date = expire_date
         self.gmail = gmail
-        self.tracking_code = #code peygiri az taraf banke 10 raghami
+        self.tracking_code = random_number_with_n_digits(10)#code peygiri az taraf banke 10 raghami
         self.order_date = None
         self.confirm = False
         #self.total_price = total_price
+
 
 
         if not isinstance(int, card_number): #validate card number
@@ -39,7 +43,7 @@ class Pay:
                 pass
 
 
-        if not isinstance(int, cvv2):
+        if not isinstance(int, cvv2): #validate cvv2
             return "Enter numbers for cvv2!"
         elif isinstance(int, cvv2):
             if (len(str(cvv2)) != 3) or (len(str(cvv2)) != 4):
