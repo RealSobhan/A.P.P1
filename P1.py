0cd55e37ecd3820b45bbe1eb5f549104afb7f4da
Sobhan <Sobhan.kasa@gmail.com>
2023-04-19 19:38:47 +0430

    add def for n digits numbers

--- P1.py
+++ P1.py
@@ -1,3 +1,5 @@
+
+
 class Inventory:
 
 class Customer:
@@ -10,13 +12,19 @@ class Product:
 class Cart:
 
 """ pay gharare oon safhe vared kardane shomare card va takmil farayand kharid ro shabih sazi kone"""
+
+
+def random_number_with_n_digits(n):
+    range_start = 10**(n-1)
+    range_end = (10**n)-1
+    return randint(range_start, range_end)
 class Pay:
     def __init__(self, card_number, cvv2, expire_date, gmail = None):
         self.card_number = card_number
         self.cvv2 = cvv2
         self.expire_date = expire_date
         self.gmail = gmail
-        self.tracking_code = None
+        self.tracking_code = #code peygiri az taraf banke 10 raghami
         self.order_date = None
         self.confirm = False
         #self.total_price = total_price
