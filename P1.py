ff625f613549296345f10bed6965f5b142ac2cd4
Ali Bozorgmehry <bozorgmehry.ali@gmail.com>
2023-04-20 17:45:23 +0430

    defining cart class

--- P1.py
+++ P1.py
@@ -6,17 +6,49 @@ class Admin:
 
 class Product:
 
-
+# ajnas dakhele cart tu ye dictionary rikhte mishan ke key haye dictionary esme jens va value haye dictionary tuple hastan
+# tuple[0] = tedad , tuple[1] = gheymate oon tedad jens
 class Cart:
+    def __init__(self) :
+        my_cart = {}
+    
+    def add_to_cart(self, item, number) : 
+        my_cart[item] = (number, number * item.price)
 
+    def remove_from_cart(self, item, number) : 
+        first_number = my_cart[item][0]
+        if number > first_number :
+            print(f"you can't remove {number} number of this item from yourcart, there was only {first_number} number in it.")
+
+        elif number == first_number :
+            del my_cart[item]
+
+        else : 
+            remaining_nmber = first_number - number
+            my_cart[item] = (remaining_nmber, remaining_nmber * item.price)
+            print(f"you removed {number} number(s) of this item and now there is {remaining_nmber} number(s) remeining in your cart")
+
+    def show_my_cart(self) :
+        my_cart.items()
+
+    def empty_my_cart(self) : 
+        my_cart.clear()
+
+    def total_cost(self) : 
+        if len(my_cart) == 0 :
+            return 0
+        else :
+            items_value = 0
+            for i in my_cart.values() :
+                items_value += i[1]
+
+            return items_value
+        
+        
 
 class Pay:
     def __init__(self, card_number, cvv2, expire_date, gmail= None):
-        self.code_peygiri = None
-        self.tarikh_sabt_sefaresh = None
-        if confirm:
-            self.code_peygiri = np.random()
-            self.tarikh_sabt_sefaresh = time.now()
+
 class Address:
     def __init__(self, sharestan, shahr, baze_tahvil, addrees_koli, code_posti, pelak, vahed, phone_number, tahvil_girande):
 
