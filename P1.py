219e51a10a3181ccdf5065757316d5ba416736da
Sobhan <Sobhan.kasa@gmail.com>
2023-04-22 12:29:08 +0430

    validate card number completed

--- P1.py
+++ P1.py
@@ -1,6 +1,6 @@
 from random import randint
 import re
-class Inventory:
+"""class Inventory:
 
 class Customer:
 
@@ -11,8 +11,43 @@ class Product:
 
 class Cart:
 
-""" pay gharare oon safhe vared kardane shomare card va takmil farayand kharid ro shabih sazi kone"""
+ pay gharare oon safhe vared kardane shomare card va takmil farayand kharid ro shabih sazi kone
 
+"""
+"""
+
+class Transaction:
+    def __init__(self):
+        self.transaction_type = self._get_transaction_type()
+        self.amount = self._get_amount()
+        self.description = self._get_description()
+
+    def _get_transaction_type(self):
+        while True:
+            transaction_type = input("Enter transaction type (debit/credit): ")
+            if transaction_type.lower() not in ['debit', 'credit']:
+                print("Error: Invalid transaction type. Please enter debit or credit.")
+                continue
+            return transaction_type.lower()
+
+    def _get_amount(self):
+        while True:
+            try:
+                amount = float(input("Enter transaction amount: "))
+            except ValueError:
+                print("Error: Invalid amount. Please enter a number.")
+                continue
+            return amount
+
+    def _get_description(self):
+        while True:
+            description = input("Enter transaction description: ")
+            if len(description) > 50:
+                print("Error: Description is too long. Please enter a shorter description.")
+                continue
+            return description
+
+"""
 
 def random_number_with_n_digits(n):
     range_start = 10**(n-1)
@@ -22,8 +57,8 @@ def random_number_with_n_digits(n):
 def validate_email(email):
     return bool(re.match(r"^[a-zA-Z0-9\.\_]+@((gmail)|(yahoo)|(outlook)|(hotmail)|(live)|([a-z]*\.*[a-z]+\.ac)|(chmail))\.((com)|(ir))$", email, re.IGNORECASE))
 class Pay:
-    def __init__(self, card_number, cvv2, expire_date, gmail = None):
-        #self.card_number = card_number
+    def __init__(self, gmail = None):
+        self.card_number = self.get_card_number()
         #self.cvv2 = cvv2
         #self.expire_date = expire_date
         #self.gmail = gmail
@@ -33,14 +68,18 @@ class Pay:
         #self.total_price = total_price
 
 
-    def validate_card_number(self, card_number):
-        if not isinstance(int, card_number): #validate card number
-            print("Enter numbers for card number!")
-        elif isinstance(int, card_number):
+    def get_card_number(self):
+        while True:
+            try:
+                card_number = int(input("Enter your card number here: ")) #Validate card number
+            except:
+                print("Error: Enter numbers for card number!")
+                continue
             if len(str(card_number)) != 16:
-                print("Enter Valid Card Number!")
+                print("Error: Enter Valid Card Number!")
+                continue
             else:
-                pass
+                return card_number
 
     def validate_cvv2(self, cvv2):
         if not isinstance(int, cvv2): #validate cvv2
@@ -48,6 +87,11 @@ class Pay:
         elif isinstance(int, cvv2):
             if (len(str(cvv2)) != 3) or (len(str(cvv2)) != 4):
                 print("Enter valid numbers for cvv2")
+            else:
+                pass
+
+"""
+    def
 
 
 
@@ -58,8 +102,12 @@ class Pay:
 
             self.code_peygiri = np.random()
             self.tarikh_sabt_sefaresh = time.now()
+"""
+"""
 class Address:
     def __init__(self, sharestan, shahr, baze_tahvil, addrees_koli, code_posti, pelak, vahed, phone_number, tahvil_girande):
 
 class Factor:
     def __init__(self, gheymat, ajnas, address,tarikh_sabt_sefaresh ):
+"""
+pay = Pay()
