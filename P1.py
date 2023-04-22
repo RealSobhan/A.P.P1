736be130bbd4761175792c7625751229678f2cdf
Sobhan <Sobhan.kasa@gmail.com>
2023-04-22 11:21:28 +0430

    change returns to prints

--- P1.py
+++ P1.py
@@ -1,5 +1,5 @@
 from random import randint
-
+import re
 class Inventory:
 
 class Customer:
@@ -23,31 +23,31 @@ def validate_email(email):
     return bool(re.match(r"^[a-zA-Z0-9\.\_]+@((gmail)|(yahoo)|(outlook)|(hotmail)|(live)|([a-z]*\.*[a-z]+\.ac)|(chmail))\.((com)|(ir))$", email, re.IGNORECASE))
 class Pay:
     def __init__(self, card_number, cvv2, expire_date, gmail = None):
-        self.card_number = card_number
-        self.cvv2 = cvv2
-        self.expire_date = expire_date
-        self.gmail = gmail
-        self.tracking_code = random_number_with_n_digits(10)#code peygiri az taraf banke 10 raghami
-        self.order_date = None
-        self.confirm = False
+        #self.card_number = card_number
+        #self.cvv2 = cvv2
+        #self.expire_date = expire_date
+        #self.gmail = gmail
+        #self.tracking_code = random_number_with_n_digits(10) #code peygiri az taraf banke 10 raghami
+        #self.order_date = None
+        #self.confirm = False
         #self.total_price = total_price
 
 
-
+    def validate_card_number(self, card_number):
         if not isinstance(int, card_number): #validate card number
-            return "Enter numbers for card number!"
+            print("Enter numbers for card number!")
         elif isinstance(int, card_number):
             if len(str(card_number)) != 16:
-                return "Enter Valid Card Number!"
+                print("Enter Valid Card Number!")
             else:
                 pass
 
-
+    def validate_cvv2(self, cvv2):
         if not isinstance(int, cvv2): #validate cvv2
-            return "Enter numbers for cvv2!"
+            print("Enter numbers for cvv2!")
         elif isinstance(int, cvv2):
             if (len(str(cvv2)) != 3) or (len(str(cvv2)) != 4):
-                return "Enter valid numbers for cvv2"
+                print("Enter valid numbers for cvv2")
 
 
 
