b4e559c591c9a953516f38a6f6168b2f15101c64
Sobhan <Sobhan.kasa@gmail.com>
2023-04-22 13:15:23 +0430

    order date completed

--- P1.py
+++ P1.py
@@ -1,6 +1,7 @@
 from random import randint
 import re
 from datetime import datetime
+
 """class Inventory:
 
 class Customer:
@@ -50,29 +51,33 @@ class Transaction:
 
 """
 
+
 def random_number_with_n_digits(n):
-    range_start = 10**(n-1)
-    range_end = (10**n)-1
+    range_start = 10 ** (n - 1)
+    range_end = (10 ** n) - 1
     return randint(range_start, range_end)
 
+
 def validate_email(email):
-    return bool(re.match(r"^[a-zA-Z0-9\.\_]+@((gmail)|(yahoo)|(outlook)|(hotmail)|(live)|([a-z]*\.*[a-z]+\.ac)|(chmail))\.((com)|(ir))$", email, re.IGNORECASE))
+    return bool(re.match(
+        r"^[a-zA-Z0-9\.\_]+@((gmail)|(yahoo)|(outlook)|(hotmail)|(live)|([a-z]*\.*[a-z]+\.ac)|(chmail))\.((com)|(ir))$",
+        email, re.IGNORECASE))
+
+
 class Pay:
-    def __init__(self, gmail = None):
+    def __init__(self):
         self.card_number = self.get_card_number()
         self.cvv2 = self.get_cvv2()
         self.expire_date = self.get_expire_date()
-        #self.gmail = gmail
-        #self.tracking_code = random_number_with_n_digits(10) #code peygiri az taraf banke 10 raghami
-        #self.order_date = None
-        #self.confirm = False
-        #self.total_price = total_price
-
+        self.tracking_code = random_number_with_n_digits(10) #code peygiri az taraf banke 10 raghami
+        self.order_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        # self.confirm = False
+        # self.total_price = total_price
 
     def get_card_number(self):
         while True:
             try:
-                card_number = int(input("Enter your card number here: ")) #Validate card number
+                card_number = int(input("Enter your card number here: "))  # Validate card number
             except ValueError:
                 print("Error: Enter numbers for card number!")
                 continue
@@ -84,7 +89,7 @@ class Pay:
     def get_cvv2(self):
         while True:
             try:
-                cvv2 = int(input("Enter your cvv2 here: ")) #Validate cvv2
+                cvv2 = int(input("Enter your cvv2 here: "))  # Validate cvv2
             except ValueError:
                 print("Error: Enter numbers for cvv2!")
                 continue
@@ -102,10 +107,12 @@ class Pay:
                 print("Error: Enter in date format!")
                 continue
             return expire_date
+
+
 pay = Pay()
 print(pay.expire_date)
 print(pay.card_number)
-print(pay.cvv2)
+print(pay.order_date)
 
 """
     def
