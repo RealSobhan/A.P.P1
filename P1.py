51aee3f2f5a9f3ecc2926b3047750172a12b3e62
Ali Bozorgmehry <bozorgmehry.ali@gmail.com>
2023-04-24 23:32:12 +0430

    defining the cart class

--- P1.py
+++ P1.py
@@ -9,40 +9,44 @@ class Product:
 # ajnas dakhele cart tu ye dictionary rikhte mishan ke key haye dictionary esme jens va value haye dictionary tuple hastan
 # tuple[0] = tedad , tuple[1] = gheymate oon tedad jens
 class Cart:
+    
     def __init__(self) :
-        my_cart = {}
+        self.my_cart = {}
+
     
     def add_to_cart(self, item, number) : 
-        my_cart[item] = (number, number * item.price)
+        self.my_cart[str(item)] = (number, float(number * item.price))
+        
 
     def remove_from_cart(self, item, number) : 
-        first_number = my_cart[item][0]
+        first_number = self.my_cart[str(item)][0]
         if number > first_number :
-            print(f"you can't remove {number} number of this item from yourcart, there was only {first_number} number in it.")
+            print(f"you can't remove {number} number of this item from your cart, there was only {first_number} number in it.")
 
         elif number == first_number :
-            del my_cart[item]
+            del self.my_cart[str(item)]
 
         else : 
             remaining_nmber = first_number - number
-            my_cart[item] = (remaining_nmber, remaining_nmber * item.price)
+            self.my_cart[str(item)] = (remaining_nmber, remaining_nmber * item.price)
             print(f"you removed {number} number(s) of this item and now there is {remaining_nmber} number(s) remeining in your cart")
 
     def show_my_cart(self) :
-        my_cart.items()
+        print(self.my_cart)
 
     def empty_my_cart(self) : 
-        my_cart.clear()
+        self.my_cart.clear()
 
     def total_cost(self) : 
-        if len(my_cart) == 0 :
+        if len(self.my_cart) == 0 :
             return 0
         else :
             items_value = 0
-            for i in my_cart.values() :
+            for i in self.my_cart.values() :
                 items_value += i[1]
 
             return items_value
+        
         
         
 
